6b8365b90d5548395fd1b5e42849bb97c8775cfc
Ed <ed.cetacean@outlook.com>
2024-07-27 21:39:12 -0700

    'ADD: Búsqueda de plantas'

--- smartGarden/components/client/home/home.js
+++ smartGarden/components/client/home/home.js
@@ -1,8 +1,54 @@
 
+// -------------------------------------------------------------------------- //
+
+import { perenual } from '../../../api/config.js';
+
 // HOME: CLIENT ------------------------------------------------------------- //
 
 export const init = () =>{
     console.log('   • Inicializando HOME (CLIENT)...');
 }
+
+// -------------------------------------------------------------------------- //
+
+
+document.getElementById('home-search-button').addEventListener('click', function() {
+    const consulta = document.getElementById('home-search-input').value;
+    searchPlants(consulta);
+});
+
+async function searchPlants(consulta) {
+    await fetch(perenual.url.api + 'species-list?page=1&key=' + perenual.url.apiKey + '&q=' + consulta)
+        .then(response => response.json())
+        .then(data => { console.log(data.data); })
+        .catch(error => { throw new Error('ERROR: ', error); });
+};
+
+function showResults(data) {
+    let resultContainer = document.getElementById('home-search-result');
+    let detailsContainer = document.getElementById('home-search-details');
+
+    // Limpia resultados previos.
+    resultContainer.innerHTML = '';
+    detailsContainer.innerHTML = '';
+
+    // Despliega los nuevos resultados.
+    data.results.forEach(item => {
+    const div = document.createElement('div');
+        div.textContent = item.name;
+        resultContainer.appendChild(div);
+
+    // If you want to show more details
+    const detailsDiv = document.createElement('div');
+        detailsDiv.textContent = `Details about ${item.name}: ${item.details}`;
+        detailsContainer.appendChild(detailsDiv);
+
+    });
+}
+
+
+
+
+
 
 // -------------------------------------------------------------------------- //
